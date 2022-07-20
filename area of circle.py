20ffe12cbcca3652ad7ab6b46184c7acb51805c9
MR-KVS <109613417+MR-KVS@users.noreply.github.com>
2022-07-20 20:59:27 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,3 @@
+r = int(input("enter the radius of the circle"))
+area = r*r*3.14
+print("the area of the circle with radius",r,"is",area)
---
+++ view.py
@@ -0,0 +1,5 @@
+u = input("enter the text:")
+if u.endswith(".py"):
+    print("the extension of the file is python")
+else:
+    print("invalid extension")
