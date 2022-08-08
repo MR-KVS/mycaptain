0dc28be7077f15d9bc1e4d2a3760e45744c80b67
MR-KVS <109613417+MR-KVS@users.noreply.github.com>
2022-08-08 18:19:47 +0530

    Add files via upload

---
+++ school administration.py
@@ -0,0 +1,13 @@
+condition = True
+
+while(condition):
+    student_info = input("enter the student information")
+    print(student_info)
+    check = input("enter YES if you want to continue or NO if you dont want to continue")
+    if check =="yes":
+        condition = True
+    else:
+        condition = false
+        
+        
+
